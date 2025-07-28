e91f3201bf3060d87ee6687b12c1a7c1d37f5566
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-28 20:39:43 +0530

    about type casting

---
+++ variable-and-dataType/typeCasting.java
@@ -0,0 +1,17 @@
+public class typeCasting {
+  
+    public static void main(String args[]){
+
+        // float to int
+        float a = 74.4f;
+        int b = (int)a;
+
+        System.out.println(b);
+
+        // double to float
+        double c = 42547.35;
+        float d = (float)c;
+
+        System.out.println(d);
+    }
+}
