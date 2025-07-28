1609779daaf4884544e5c122d442b43380cfcb37
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-28 15:16:00 +0530

    Area of circle by given radius

---
+++ variable-and-dataType/areaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class areaOfCircle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of radius : ");
+        float radius = sc.nextFloat();
+
+        float area = 3.14f *radius*radius;
+
+        System.out.println("Area of circle is : "+area );
+
+    }
+}
