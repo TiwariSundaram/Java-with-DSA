00df773df56047f12a3a9d8b852544749eba926b
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-28 15:02:29 +0530

    multiplication of twon integer no.

---
+++ variable-and-dataType/mul.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class mul{
+   public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+
+     System.out.println("enter your first no.");
+     int a = sc.nextInt();
+
+     System.out.println("enter your second no.");
+     int b = sc.nextInt();
+
+     int mul = a * b;
+
+     System.out.print("mul of "+a+" and "+b +" is "+mul);
+    }
+}
