4a32eb059e9e3c2ec5404843175fa568a464e3d2
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-28 15:23:39 +0530

    about type conversion

---
+++ variable-and-dataType/typeConversion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class typeConversion {
+    Scanner sc = new Scanner(System.in);
+
+    // byte => short => int => float => long => double
+    // source type  < destination type
+
+    int a = 9;
+    float b =a;
+
+    // invalid syntax => lossy conversion
+
+   /*  float c = 9.8f;
+    int d = c;
+    */
+
+
+}
