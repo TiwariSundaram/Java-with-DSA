0d6e579950a4d67370a3193bae62f3ca73436940
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-30 01:08:20 +0530

    code for turnary operator

---
+++ conditional-statement/turnaryOperator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class turnaryOperator {
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your marks : ");
+        int marks = sc.nextInt();
+
+        String result = (marks>=33)?"pass":"fail";
+
+        System.out.println(result);
+    }
+}
