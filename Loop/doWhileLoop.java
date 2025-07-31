74429b483dc96415021718f9f83e7e4a54ff29cf
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-31 12:15:47 +0530

    code for do while loop

---
+++ Loop/doWhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class doWhileLoop {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the num :");
+        int a = sc.nextInt();
+
+        int count = 0;
+
+        do { 
+            System.out.println("jay shree Ram");
+            count++;
+        } while (count<a);
+
+    }
+    
+}
