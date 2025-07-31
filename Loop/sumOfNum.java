218aba539372f7d074fdf9de32c7272f38f9a8a3
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-07-31 11:15:37 +0530

    sum of natural number

---
+++ Loop/sumOfNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class sumOfNum {
+
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the num from where you want to start sum : ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the num as for as you want to start sum : ");
+        int b = sc.nextInt();
+
+        int sum = 0;
+
+        while (a<=b){
+            sum += a;
+            a++;  
+        }
+        System.out.println("total sum = "+ sum);
+    }
+    
+}
