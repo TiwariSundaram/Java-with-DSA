477d4a237eb010a34e7b18c85c748c3f054e96cd
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-08-01 22:22:49 +0530

    code for find the factorial value of any number

---
+++ Loop/factorialNum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class factorialNum{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number for factorial value : ");
+        int  num = sc.nextInt();
+        int factValue = 1;
+
+        if(num == 0){
+            factValue = 0;
+        }else{
+            for (int i = 1; i<=num ; i++){
+            factValue *= i ;
+        }
+        }
+        
+        System.out.println(num +"! = "+factValue);
+    }
+}
