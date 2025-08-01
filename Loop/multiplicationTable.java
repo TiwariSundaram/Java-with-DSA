36d7d6f078aa5e756988282023d7e6cf90101eef
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-08-01 23:26:46 +0530

    code for multiplication table of num

---
+++ Loop/multiplicationTable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class multiplicationTable {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the number for table : ");
+        int num = sc.nextInt();
+
+        for(int i = 1; i<= 10 ; i++){
+            int value = num*i;
+            System.out.println(num +"*"+i + " = "+ value);
+        }
+
+
+    }
+    
+}
