0dea89eca0cef71605a690f6d55d61878af232d1
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-08-02 23:18:11 +0530

    code for top Right triangle pater printing

---
+++ pattern/topRightTriangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class topRightTriangle {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the row value : ");
+        int row = sc.nextInt();
+
+        System.out.println("Enter the column value : ");
+        int column= sc.nextInt();
+
+        for(int i =1 ; i<=row ;i++){
+            for (int j = 0; j <=column; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
