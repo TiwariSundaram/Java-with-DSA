b68b8ca14de21323310a77fd2ab5f2bd4e252416
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-08-03 18:15:41 +0530

    code for bottom left triangle pattern printing

--- pattern/bottomLeftTriangle.java
+++ pattern/bottomLeftTriangle.java
@@ -5,20 +5,17 @@ public class bottomLeftTriangle {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the row value");
+        System.out.println("Enter the row value : ");
         int row = sc.nextInt();
 
         System.out.println("Enter the column value : ");
         int column= sc.nextInt();
 
         for(int i =1 ; i<=row ;i++){
-            for (int j = i; j <= column; j++) {
+            for (int j = 1; j <= column; j++) {
                 if((i-j)>=0){
-                    System.out.print("* ");
-                }
-                else{
-                    System.out.print(" ");
-                }                
+                    System.out.print("*");
+                }             
             }
             System.out.println();
         }
