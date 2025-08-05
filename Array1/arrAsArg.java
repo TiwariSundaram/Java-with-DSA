4c85983a1e9b819c75ef5c8ce77ee1480d5d4875
TiwariSundaram <tiwarisundaram089@gmail.com>
2025-08-05 22:14:09 +0530

    int this code passing array as a argument

---
+++ Array1/arrAsArg.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class arrAsArg {
+
+    public static int[] updateArr(int marks[],Scanner sc){
+
+        System.out.println("enter the value that you want to add in each element of marks:");
+        int n = sc.nextInt();
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i]+n;
+        }
+        return marks;
+    }
+
+    public static int []createArray(int marks[],Scanner sc){
+
+
+        System.out.println("Enter the marks : ");
+
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = sc.nextInt();
+        }
+        return marks;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the length of marks array : ");
+        int length = sc.nextInt();
+
+        int marks[] = new int[length];
+
+       int array []= createArray(marks, sc);
+
+       System.out.println("Array is : ");
+       for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] +" ");
+        }
+        System.out.println();
+
+        int updatedArray[] = updateArr(marks,sc);
+
+        System.out.println("updated Array is : ");
+        for (int i = 0; i < updatedArray.length; i++) {
+            System.out.print(updatedArray[i] +" ");
+        }
+
+        
+
+        
+        
+    }
+    
+}
